4bbdf8175daec885edd103d41be60ed070cd2a3f
Blake Pastrana <blake.pastrana@accenturefederal.com>
2018-04-21 21:30:13 -0400

    bleh

--- src/pages/medals/medals.ts
+++ src/pages/medals/medals.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { User } from '../../providers/providers';
 
 /**
  * Generated class for the MedalsPage page.
@@ -15,11 +17,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MedalsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	medal1: number;
+	username: string;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MedalsPage');
+    this.username = this.username = localStorage.getItem("username");
+    this.getExercises().then((val) => {
+    	console.log(val);
+    	this.medal1 = Object.keys(val).length;
+        });
   }
 
+  getExercises(): Promise<any> {
+    return this.storage.get(this.username + '/exercises');
+	}
 }
