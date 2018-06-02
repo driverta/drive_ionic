a43a317189cf811caa5e3724eb31a787fd7485ce
Blake Pastrana <blake.pastrana@accenturefederal.com>
2018-06-02 11:54:29 -0400

    Fixed error from TD

--- src/pages/friend-cardio-record-detail/friend-cardio-record-detail.module.ts
+++ src/pages/friend-cardio-record-detail/friend-cardio-record-detail.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { FriendCardioRecordDetailPage } from './friend-cardio-record-detail';
-
-import { FriendCardioProfileRecordsComponent } from '../../components/friend-cardio-profile-records/friend-cardio-profile-records';
-import { FriendCardioProfileBarComponent } from '../../components/friend-cardio-profile-bar/friend-cardio-profile-bar';
 import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
-    FriendCardioRecordDetailPage,
-    FriendCardioProfileRecordsComponent,
-    FriendCardioProfileBarComponent
+    FriendCardioRecordDetailPage
   ],
   imports: [
     IonicPageModule.forChild(FriendCardioRecordDetailPage),
