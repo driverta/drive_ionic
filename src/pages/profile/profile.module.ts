1e0a79ad8fab2239bb61dce95f0778053ba1cb77
Blake Pastrana <blake.pastrana@accenturefederal.com>
2018-08-26 12:07:27 -0400

    Began updating UI of profile page

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SettingsPage } from './profile';
 
 @NgModule({
@@ -10,7 +10,16 @@ import { SettingsPage } from './profile';
   ],
   imports: [
     IonicPageModule.forChild(SettingsPage),
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    }),
   ],
   exports: [
     SettingsPage
